bd6c75b752203e2b04a3aafec38f6eb2c3cf065e
erick2990 <e30rick@gmail.com>
2025-07-09 17:00:26 -0600

    unificar

--- principal.py
+++ principal.py
@@ -1,4 +1,39 @@
-print('\r\t\t===Bienvenido usuario a clinica medica ===')
+from time import process_time_ns
+import recepcion
+import Farmacia
+from Farmacia import ingresoMedicamentos, EntrgarMedicamentos, mostrarMedicamentos
 
+from recepcion import agregar_paciente, atender_paciente, mostrar_cola
 
+fin_menu =True
+
+while fin_menu:
+    try:
+        print('\r\t\t===Bienvenido usuario a clinica medica ===')
+        print('1.Agregar paciente a la cola \r\n2.Atender pacientes \r\n3.Historial pacientes')
+        print('4.Agregar medicamento \r\n5.Entregar medicamento \r\n6.Mostrar medicamentos')
+        print('7.Salir \r\nSeleccione una opcion: ')
+        op = int(input())
+
+        match op:
+            case 1:
+                agregar_paciente()
+            case 2:
+                atender_paciente()
+            case 3:
+                mostrar_cola()
+            case 4:
+                ingresoMedicamentos()
+            case 5:
+                EntrgarMedicamentos()
+            case 6:
+                mostrarMedicamentos()
+            case 7:
+                print('Saliendo del programa....')
+                fin_menu =False
+            case _:
+                print('Error opcion incorrecta')
+
+    except ValueError:
+        print('Error valor inadecuado')
 
