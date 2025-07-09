d413d923f561a16600656f799a2fadee7e3ae93b
erick2990 <e30rick@gmail.com>
2025-07-09 10:45:42 -0600

    Funcion para agregar, atender y mostrar la cola de los pacientes

--- farmacia.py
+++ farmacia.py
@@ -3,14 +3,29 @@ from collections import deque
 pacientes = deque() #esto es la cola de los productos de la farmacia
 
 
+def mostrar_cola():
+    cont =1
+    print('Cola de pacientes')
+    for x in pacientes:
+        print(f'{cont} Nombre: {x}')
+        cont+=1
+
+
 
 def agregar_paciente():
     fin = True
     while fin:
         try:
             nombre_Paciente = input('Ingrese el nombre del paciente:')
+            pacientes.append(nombre_Paciente) #Se añade un paciente
+            print('Paciente añadido con exito...')
+            fin = False
 
         except ValueError:
-            print('Error ingreso un dato incorrecto')
+            print('Error ingreso un dato incorrecto vuelva a intetntarlo')
 
 
+
+def atender_paciente():
+    fin_atender  = True
+    print('Se atendera a los pacientes en el orden que entraron: ')
